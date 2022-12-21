3a75f22125f60348af91769fc3c3354c05a98729
HoodFast <rabota-trassa@mail.ru>
2022-12-21 19:00:12 +0300

    commit

--- EnumerableMagic#20.js
+++ EnumerableMagic#20.js
@@ -1,6 +1,6 @@
 function eachCons(array, n) {
     const result = [];
-      for (i = 0; i <= array.length - n; i++){
+      for (let i = 0; i <= array.length - n; i++){
       result.push(array.slice(i, i+n));
     }
     return result
