1c32d4dd2735426c3417a087d1c8ca881949e133
HoodFast <rabota-trassa@mail.ru>
2022-12-25 20:08:59 +0300

    commit

--- Highest and Lowest.js
+++ Highest and Lowest.js
@@ -1,9 +1,5 @@
 function highAndLow(numbers){
-  
-    let nums = numbers.split(" ");
-    for(var i = 0; i < nums.length; i++) {
-       nums[i] = Number(nums[i]);
-  }
+    let nums = numbers.split(" ").map(Number);
     let res = Math.max.apply(null, nums).toString() + ' ' + Math.min.apply(null, nums).toString();
     return res
   }
