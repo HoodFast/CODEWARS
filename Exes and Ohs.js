fdc6a55c179db88480343d26880cc3c6b1323cf6
HoodFast <rabota-trassa@mail.ru>
2023-10-08 23:33:58 +0300

    commit

--- Exes and Ohs.js
+++ Exes and Ohs.js
@@ -2,7 +2,7 @@ function XO(str) {
     if(!str.length){return true}
 
     let temp = str.toUpperCase().split('')
-    console.log(temp)
+
     if(temp.filter(i=>i==='O').length===0 || temp.filter(i=>i==='X').length===0){
         return false
     }
